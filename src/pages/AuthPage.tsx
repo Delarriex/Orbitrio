c49e1b197bf79c2fec224da303d8d47a4070a50b
Delarriex <uaemiratepost@gmail.com>
2026-06-25 10:41:01 -0700

    Update AuthPage.tsx

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -763,7 +763,7 @@ useEffect(() => {
                   </button> 
                 </div> 
              </div>  
-        ) : null}
+        ) } null}
                 
         {/* Secure signpost */}
         <div className="pt-8 border-t border-orbit-border/15 mt-8 text-center space-y-2 text-[10px] text-zinc-500">
